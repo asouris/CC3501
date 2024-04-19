f76d729ef199b1c6ed8805aaf03af5339ca8559c
juju <a-github@asouris.com>
2024-04-18 21:05:32 -0400

    Aux 4

--- auxiliares/utils/helpers.py
+++ auxiliares/utils/helpers.py
@@ -2,27 +2,15 @@ import numpy as np
 import trimesh as tm
 from OpenGL import GL
 import utils.transformations as tr
+import networkx as nx
 
 
 # Clase Model() nos ayuda a transformar las figuras de forma mas facil
-# Se inicializa con las posiciones de los vertices, los colores de los vertices, un alpha y la index_data si es que
+# Se inicializa con las posiciones de los vertices, los colores de los vertices y la index_data si es que
 # tenemos una figura con vertices indexados
 class Model():
-
-    # Constructor
-    def __init__(self, position_data, color_data, alpha, index_data=None):
-        vertices = len(color_data)//3
+    def __init__(self, position_data, index_data=None):
         self.position_data = position_data
-        self.color_data = [1]*(vertices*4)
-        j = 0
-        i = 0
-        while i < vertices:
-            self.color_data[j] = color_data[i*3]
-            self.color_data[j+1] = color_data[i*3+1]
-            self.color_data[j+2] = color_data[i*3+2]
-            self.color_data[j+3] = alpha
-            i += 1
-            j += 4
 
         self.index_data = index_data
         if index_data is not None:
@@ -30,12 +18,8 @@ class Model():
 
         self.gpu_data = None
 
-        self.position = np.array([0, 0, 0], dtype=np.float32)
-        self.rotation = np.array([0, 0, 0], dtype=np.float32)
-        self.scale = np.array([1, 1, 1], dtype=np.float32)
-
-    # Crea el vertex_list
     def init_gpu_data(self, pipeline):
+        self.pipeline = pipeline
         if self.index_data is not None:
             self.gpu_data = pipeline.vertex_list_indexed(
                 len(self.position_data) // 3, GL.GL_TRIANGLES, self.index_data)
@@ -44,32 +28,17 @@ class Model():
                 len(self.position_data) // 3, GL.GL_TRIANGLES)
 
         self.gpu_data.position[:] = self.position_data
-        self.gpu_data.color[:] = self.color_data
 
-    # Dibuja en pantalla ( queremos llamar a esto en cada refresh )
     def draw(self, mode=GL.GL_TRIANGLES):
         self.gpu_data.draw(mode)
-
-    # Retorna la matriz de transformacion ( vamos a querer usar esto en cada refresh )
-    def get_transform(self):
-        translation_matrix = tr.translate(
-            self.position[0], self.position[1], self.position[2])
-        rotation_matrix = tr.rotationX(
-            self.rotation[0]) @ tr.rotationY(self.rotation[1]) @ tr.rotationZ(self.rotation[2])
-        scale_matrix = tr.scale(self.scale[0], self.scale[1], self.scale[2])
-        transformation = translation_matrix @ rotation_matrix @ scale_matrix
-        return np.reshape(transformation, (16, 1), order="F")
 
 
 # La clase Mesh(Model) es un Model pero con funciones extra,
 # Nos permite pasar un mesh hecho en algun software de modelado a OpenGL
 # Recibe el path hacia el file, y un color base opcional
 class Mesh(Model):
-    def __init__(self, asset_path, alpha, base_color=None):
-        # se carga con trimesh
+    def __init__(self, asset_path):
         mesh_data = tm.load(asset_path)
-
-        # se deja normalito con las transformaciones
         mesh_scale = tr.uniformScale(2.0 / mesh_data.scale)
         mesh_translate = tr.translate(*-mesh_data.centroid)
         mesh_data.apply_transform(mesh_scale @ mesh_translate)
@@ -77,14 +46,143 @@ class Mesh(Model):
         indices = vertex_data[3]
         positions = vertex_data[4][1]
 
-        # cantidad de vertices
-        count = len(positions) // 3
+        super().__init__(positions, indices)
 
-        # asignar colores
-        colors = []
-        if base_color is None:
-            colors = vertex_data[5][1]
-        else:
-            colors = np.array(base_color*count, dtype=np.float32)
 
-        super().__init__(positions, colors, alpha, indices)
+class SceneGraph():
+    def __init__(self, camera=None):
+        self.graph = nx.DiGraph(root="root")
+        self.add_node("root")
+        self.camera = camera
+
+    def add_node(self,
+                 name,
+                 attach_to=None,
+                 mesh=None,
+                 color=[1, 1, 1],
+                 transform=tr.identity(),
+                 position=[0, 0, 0],
+                 rotation=[0, 0, 0],
+                 scale=[1, 1, 1],
+                 mode=GL.GL_TRIANGLES):
+        self.graph.add_node(
+            name,
+            mesh=mesh,
+            color=color,
+            transform=transform,
+            position=np.array(position, dtype=np.float32),
+            rotation=np.array(rotation, dtype=np.float32),
+            scale=np.array(scale, dtype=np.float32),
+            mode=mode)
+        if attach_to is None:
+            attach_to = "root"
+
+        self.graph.add_edge(attach_to, name)
+
+    def __getitem__(self, name):
+        if name not in self.graph.nodes:
+            raise KeyError(f"Node {name} not in graph")
+
+        return self.graph.nodes[name]
+
+    def __setitem__(self, name, value):
+        if name not in self.graph.nodes:
+            raise KeyError(f"Node {name} not in graph")
+
+        self.graph.nodes[name] = value
+
+    def get_transform(self, node):
+        node = self.graph.nodes[node]
+        transform = node["transform"]
+        translation_matrix = tr.translate(
+            node["position"][0], node["position"][1], node["position"][2])
+        rotation_matrix = tr.rotationX(node["rotation"][0]) @ tr.rotationY(
+            node["rotation"][1]) @ tr.rotationZ(node["rotation"][2])
+        scale_matrix = tr.scale(
+            node["scale"][0], node["scale"][1], node["scale"][2])
+        return transform @ translation_matrix @ rotation_matrix @ scale_matrix
+
+    def draw(self):
+        root_key = self.graph.graph["root"]
+        edges = list(nx.edge_dfs(self.graph, source=root_key))
+        transformations = {root_key: self.get_transform(root_key)}
+
+        for src, dst in edges:
+            current_node = self.graph.nodes[dst]
+
+            if not dst in transformations:
+                transformations[dst] = transformations[src] @ self.get_transform(
+                    dst)
+
+            if current_node["mesh"] is not None:
+                current_pipeline = current_node["mesh"].pipeline
+                current_pipeline.use()
+
+                if self.camera is not None:
+                    if "u_view" in current_pipeline.uniforms:
+                        current_pipeline["u_view"] = self.camera.get_view()
+
+                    if "u_projection" in current_pipeline.uniforms:
+                        current_pipeline["u_projection"] = self.camera.get_projection(
+                        )
+
+                current_pipeline["u_transform"] = np.reshape(
+                    transformations[dst], (16, 1), order="F")
+
+                if "u_color" in current_pipeline.uniforms:
+                    current_pipeline["u_color"] = np.array(
+                        current_node["color"], dtype=np.float32)
+                current_node["mesh"].draw(current_node["mode"])
+
+
+class Camera():
+    def __init__(self, width, height, camera_type="perspective"):
+        self.position = np.array([1, 0, 0], dtype=np.float32)
+        self.focus = np.array([0, 0, 0], dtype=np.float32)
+        self.type = camera_type
+        self.width = width
+        self.height = height
+
+    def update(self):
+        pass
+
+    def get_view(self):
+        lookAt_matrix = tr.lookAt(
+            self.position, self.focus, np.array([0, 1, 0], dtype=np.float32))
+        return np.reshape(lookAt_matrix, (16, 1), order="F")
+
+    def get_projection(self):
+        if self.type == "perspective":
+            perspective_matrix = tr.perspective(
+                90, self.width / self.height, 0.01, 100)
+        elif self.type == "orthographic":
+            depth = self.position - self.focus
+            depth = np.linalg.norm(depth)
+            perspective_matrix = tr.ortho(-(self.width/self.height) * depth,
+                                          (self.width/self.height) * depth, -1 * depth, 1 * depth, 0.01, 100)
+        return np.reshape(perspective_matrix, (16, 1), order="F")
+
+    def resize(self, width, height):
+        self.width = width
+        self.height = height
+
+
+class OrbitCamera(Camera):
+    def __init__(self, distance, width, height, camera_type="perspective"):
+        super().__init__(width, height, camera_type)
+        self.distance = distance
+        self.phi = 0
+        self.theta = np.pi / 2
+        self.update()
+
+    def update(self):
+        if self.theta > np.pi:
+            self.theta = np.pi
+        elif self.theta < 0:
+            self.theta = 0.0001
+
+        self.position[0] = self.distance * \
+            np.sin(self.theta) * np.sin(self.phi)
+        self.position[1] = self.distance * np.cos(self.theta)
+        self.position[2] = self.distance * \
+            np.sin(self.theta) * np.cos(self.phi)
--- auxiliares/utils/shapes.py
+++ auxiliares/utils/shapes.py
@@ -3,6 +3,19 @@ RED = [1, 0, 0]
 GREEN = [0, 1, 0]
 BLUE = [0, 0, 1]
 
+CYAN = [0, 1, 1]
+MAGENTA = [1, 0, 1]
+YELLOW = [1, 1, 0]
+
+WHITE = [1, 1, 1]
+BLACK = [0, 0, 0]
+
+GRAY = [0.5, 0.5, 0.5]
+ORANGE = [1, 0.5, 0]
+BROWN = [0.5, 0.25, 0]
+LIGHT_BLUE = [0.5, 0.5, 1]
+DARK_BLUE = [0, 0, 0.5]
+
 Axes = {
     'position': [
         -10, 0, 0,
@@ -106,4 +119,133 @@ Square = {
         *RED,
         *RED
     ]
+}
+
+
+Cube = {
+    'position': [
+        # Cara frontal
+        -0.5, -0.5, 0.5,
+        0.5, -0.5, 0.5,
+        0.5,  0.5, 0.5,
+        -0.5,  0.5, 0.5,
+        # Cara trasera
+        0.5, -0.5, -0.5,
+        -0.5, -0.5, -0.5,
+        -0.5,  0.5, -0.5,
+        0.5,  0.5, -0.5,
+        # Cara izquierda
+        -0.5, -0.5, -0.5,
+        -0.5, -0.5,  0.5,
+        -0.5,  0.5,  0.5,
+        -0.5,  0.5, -0.5,
+        # Cara derecha
+        0.5, -0.5,  0.5,
+        0.5, -0.5, -0.5,
+        0.5,  0.5, -0.5,
+        0.5,  0.5,  0.5,
+        # Cara superior
+        -0.5,  0.5,  0.5,
+        0.5,  0.5,  0.5,
+        0.5,  0.5, -0.5,
+        -0.5,  0.5, -0.5,
+        # Cara inferior
+        0.5, -0.5,  0.5,
+        -0.5, -0.5,  0.5,
+        -0.5, -0.5, -0.5,
+        0.5, -0.5, -0.5],
+    'color': [
+        # Cara frontal
+        *BLUE,
+        *BLUE,
+        *BLUE,
+        *BLUE,
+        # Cara trasera
+        *YELLOW,
+        *YELLOW,
+        *YELLOW,
+        *YELLOW,
+        # Cara izquierda
+        *CYAN,
+        *CYAN,
+        *CYAN,
+        *CYAN,
+        # Cara derecha
+        *RED,
+        *RED,
+        *RED,
+        *RED,
+        # Cara superior
+        *GREEN,
+        *GREEN,
+        *GREEN,
+        *GREEN,
+        # Cara inferior
+        *MAGENTA,
+        *MAGENTA,
+        *MAGENTA,
+        *MAGENTA],
+    'uv': [
+        # Cara frontal
+        0, 0,
+        1, 0,
+        1, 1,
+        0, 1,
+        # Cara trasera
+        0, 0,
+        1, 0,
+        1, 1,
+        0, 1,
+        # Cara izquierda
+        0, 0,
+        1, 0,
+        1, 1,
+        0, 1,
+        # Cara derecha
+        0, 0,
+        1, 0,
+        1, 1,
+        0, 1,
+        # Cara superior
+        0, 0,
+        1, 0,
+        1, 1,
+        0, 1,
+        # Cara inferior
+        0, 0,
+        1, 0,
+        1, 1,
+        0, 1],
+    'normal': [
+        # Cara frontal
+        *([0, 0, 1]*4),
+        # Cara trasera
+        *([0, 0, -1]*4),
+        # Cara izquierda
+        *([-1, 0, 0]*4),
+        # Cara derecha
+        *([1, 0, 0]*4),
+        # Cara superior
+        *([0, 1, 0]*4),
+        # Cara inferior
+        *([0, -1, 0]*4)],
+    'indices': [
+        # Cara frontal
+        0, 1, 2,
+        2, 3, 0,
+        # Cara trasera
+        4, 5, 6,
+        6, 7, 4,
+        # Cara izquierda
+        8, 9, 10,
+        10, 11, 8,
+        # Cara derecha
+        12, 13, 14,
+        14, 15, 12,
+        # Cara superior
+        16, 17, 18,
+        18, 19, 16,
+        # Cara inferior
+        20, 21, 22,
+        22, 23, 20]
 }
